0aee29b0259395cf293e208168e252e6e4050707
amirka7777 <amirmgoian0808@gmail.com>
2026-02-07 14:27:02 +0700

    measured the execution time

--- university-labs/laba__12/main.c
+++ university-labs/laba__12/main.c
@@ -1,9 +1,12 @@
 #include "carsHeader.h"
 #include <stdio.h>
+#include <sys/time.h>
 
 int main() {
 
     Cars carsArray[N];
+    struct timeval start, end;
+    long seconds, microSeconds;
     
     ArrayFilling(carsArray);
 
@@ -15,5 +18,18 @@ int main() {
     printf("массив после сортировки по году выпуска:\n");
     bubbleSort(carsArray);
     printArray(carsArray);
+    printf("\n");
 
+    printf("подсчет время выполнения сортировки:\n");
+    gettimeofday(&start, NULL);
+
+    bubbleSort(carsArray);
+
+    gettimeofday(&end, NULL);
+    seconds = end.tv_sec - start.tv_sec;
+    microSeconds = end.tv_usec - start.tv_usec;
+
+    double total_seconds = seconds + microSeconds / 1000000.0;
+
+    printf("время выполнения функции (с): %.5f \n", total_seconds);
 }
