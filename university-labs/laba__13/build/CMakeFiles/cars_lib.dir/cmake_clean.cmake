7db290bf9e1138dfc01fe1188b114b13942e947a
amirka77777 <amirmgoian0808@gmail.com>
2026-02-20 21:02:40 +0700

    done for 4

---
+++ university-labs/laba__13/build/CMakeFiles/cars_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cars_lib.dir/src/carsFunction.c.o"
+  "CMakeFiles/cars_lib.dir/src/carsFunction.c.o.d"
+  "libcars_lib.pdb"
+  "libcars_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cars_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
